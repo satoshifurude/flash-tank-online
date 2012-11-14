3368f28f5c7a3117e11cade9af40d33d314d33d7
nguyentanloc109@gmail.com <nguyentanloc109@gmail.com@8ab8e3e1-8274-bc8a-e61e-904bd1191da7>
2012-11-14 09:43:51 +0000

    - update build file

--- trunk/ flash-tank-online/make.bat
+++ trunk/ flash-tank-online/make.bat
@@ -5,11 +5,33 @@ cls
 
 REM : Load config
 call config.bat
+ECHO startmake
 
 if "%1"=="debug" (
-	set MAKE_DEBUG = 1
+	set MAKE_DEBUG=1
+	set MAKE_RUN=
+	set MAKE_BUILD=
+) else if "%1"=="run" (
+	set MAKE_DEBUG=
+	set MAKE_RUN=1
+	set MAKE_BUILD=
+) else if "%1"=="build" (
+	set MAKE_DEBUG=
+	set MAKE_RUN=
+	set MAKE_BUILD=1
 )
 
+
+if "%MAKE_DEBUG%"=="1" (	
+	echo ...make debug
+	REM %FLEX_DIR%\bin\fdb %OUTPUT_SWF%
+	D:\Work\flex_sdk_4.6.0.23201\bin\fdb D:\D1\client\_release\DragonIsland_ZINGME_1_0.3.0.swf
+) else if "%MAKE_RUN%"=="1" (
+	echo ...make run
+	REM %FLASH_DEBUG% %OUTPUT_SWF%
+	call D:\D1\client\FlashPlayerDebugger.exe D:\D1\client\_release\DragonIsland_ZINGME_1_0.3.0.swf
+) else if "%MAKE_BUILD%"=="1" (
+	echo ...make build
+	%FLEX_DIR%\bin\mxmlc.exe -target-player %TARGET_FLASH_PLAYER% -compiler.library-path %FLEX3_LIB% -debug=true main.as -static-link-runtime-shared-libraries=true
 )
 
-%FLEX_DIR%\bin\mxmlc.exe -compiler.include-libraries=%STARLING_LIB% -static-link-runtime-shared-libraries=true %MAIN_DIR%
